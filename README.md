6fd36d1062253b779eb5600da72c53c894ac9903
Just1Developer <weisserbenjamin98@gmail.com>
2024-06-12 15:18:50 +0200

    Cleanup

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
-# Ich bin schon fertig mit Proggen
+# Ich bin schon fertig mit Proggen (2,3)
 
-Die Aufgabe lässt sich runter reduzieren auf mehrfache Breitensuche eines Graphen bis jeder Knoten besucht wurde. 
+### Notiz für Tutoren: Diese Aufgaben mache ich nur zum Zeitvertreib. Bewerten oder großartig analysieren braucht ihr nicht (mehr als notwendig).
+
+Die # von Droiden sind so, dass sie nur von Planeten ausgehen müssen, die sonst nicht erreicht werden.
+Unter den gegebenen Bedingungen sind das genau alle Planeten mit Eingangsgrad 0.
+
+Beim Einlesen müssen wir also nur markieren, welcher Planet besucht wird. Am Ende gehen wir alle durch, und schauen, wie viele Planeten im Endeffekt nicht besucht werden.
+Genau auf so vielen Planeten muss dann ein Droide losgeschickt werden.
--- src/edu/kit/kastel/OneAndOnly.java
+++ src/edu/kit/kastel/OneAndOnly.java
@@ -1,13 +1,6 @@
 package edu.kit.kastel;
 
-import java.util.ArrayList;
 import java.util.List;
-/*
-import java.util.Collections;
-import java.util.Comparator;
-import java.util.LinkedList;
-import java.util.Queue;
- */
 
 /**
  * The one and only class. Solves the problem entirely.
@@ -20,9 +13,7 @@ public final class OneAndOnly {
     private static final int FILEPATH_INDEX = 0;
     private static final int TOTAL_SUM_INDEX = 0;
 
-    private static final List<List<Integer>> HYPERROUTES = new ArrayList<>();
-    private static int[] entryDegrees;
-    private static int planetCount;
+    private static boolean[] hasEntryPoints;
 
     private OneAndOnly() { }
 
@@ -32,68 +23,27 @@ public final class OneAndOnly {
      */
     public static void main(String[] args) {
         List<String> filecontents = FileHelper.readAllLines(args[FILEPATH_INDEX]);
-        planetCount = Integer.parseInt(filecontents.get(TOTAL_SUM_INDEX));
-        entryDegrees = new int[planetCount];
+        int planetCount = Integer.parseInt(filecontents.get(TOTAL_SUM_INDEX));
+        hasEntryPoints = new boolean[planetCount];
+
         for (int i = 1; i <= planetCount; ++i) {
             String[] numbers = filecontents.get(i).split(NUMBER_SEPARATOR);
             int routeCount = Integer.parseInt(numbers[TOTAL_SUM_INDEX]);
-            List<Integer> planetRoutes = new ArrayList<>();
             for (int j = 0; j < routeCount; ++j) {
                 int targetPlanet = Integer.parseInt(numbers[j + 1]);
-                planetRoutes.add(targetPlanet);
-                ++entryDegrees[targetPlanet];
+                hasEntryPoints[targetPlanet] = true;
             }
-            HYPERROUTES.add(planetRoutes);
         }
         System.out.println(getMinDroidCount());
     }
 
     private static int getMinDroidCount() {
         int droids = 0;
-        for (int i : entryDegrees) {
-            if (i == 0) {
-                droids++;
+        for (boolean hasEntries : hasEntryPoints) {
+            if (!hasEntries) {
+                ++droids;
             }
         }
         return droids;
-        /*
-        List<Integer> remainingPlanets = new ArrayList<>();
-        for (int i = 0; i < planetCount; ++i) {
-            remainingPlanets.add(i);
-        }
-        // Sort by entry degrees.
-        remainingPlanets.sort(Comparator.comparingInt(planet -> ENTRY_DEGREES[planet]));
-        // In theory, everything with entry degree >= 1 will be reached by something else, so we really just need to
-        // loop through all with entry degree == 0.
-
-        while (!remainingPlanets.isEmpty()) {
-            // Choose a new start planet
-            int startPlanet = remainingPlanets.get(0);
-            //remainingPlanets.remove((Object) startPlanet);
-            //Queue<Integer> placesIWantToSee = new LinkedList<>(HYPERROUTES.get(startPlanet));
-            Queue<Integer> placesIWantToSee = new LinkedList<>();
-            placesIWantToSee.add(startPlanet);
-            System.out.println("Starting with planet " + startPlanet);
-            while (!placesIWantToSee.isEmpty()) {
-                int newPlanet = placesIWantToSee.poll();
-                // Todo debug
-                System.out.println("Attempting to remove planet with id %d, success will be: %s"
-                    .formatted(newPlanet, placesIWantToSee.contains((Object) newPlanet)));
-                // This cast to object is necessary because otherwise we're calling
-                // list.remove(index), which is a) not what we want and b) returns a T, not a boolean.
-                if (remainingPlanets.remove((Object) newPlanet)) {
-                    // We haven't visited before
-                    // Enqueue all objects. If we've seen them before, we won't end up when they're dequeued.
-                    List<Integer> routes = HYPERROUTES.get(startPlanet);
-                    placesIWantToSee.addAll(routes);
-                    // Todo debug
-                    for (int i : routes) System.out.println("Encountered Planet %d, will be removing it shortly".formatted(i));
-                }
-            }
-            // We have visited this entire graph. Now we add the 1 droid that will explore it.
-            droids++;
-        }
-        return droids;
-         */
     }
 }
