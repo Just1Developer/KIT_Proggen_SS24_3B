2a6ce0df7d95aca3672c7c13b23fe5b6489e3e86
Just1Developer <weisserbenjamin98@gmail.com>
2024-06-13 09:53:33 +0200

    O(n*m)

--- src/edu/kit/kastel/OneAndOnly.java
+++ src/edu/kit/kastel/OneAndOnly.java
@@ -1,6 +1,5 @@
 package edu.kit.kastel;
 
-import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -18,20 +17,24 @@ public final class OneAndOnly {
     private OneAndOnly() { }
 
     /**
-     * The main entry point.
+     * The main entry point and sole method of this program.<br/>
+     * <br/>
+     * Solution is in O(n*m) time, where n = number of planets and m = number of hyperroutes per planet.<br/>
+     * This time estimate includes processing the file contents, but not implicit array initialization
+     * or reading the file contents from the hard drive.
      * @param args Command line arguments.
      */
     public static void main(String[] args) {
         // Read file contents
-        List<String> fileContents = FileHelper.readAllLines(args[FILEPATH_INDEX]);
+        final List<String> fileContents = FileHelper.readAllLines(args[FILEPATH_INDEX]);
         // Get the number of planets by parsing the first line to an integer
-        int planetCount = Integer.parseInt(fileContents.get(TOTAL_SUM_INDEX));
+        final int planetCount = Integer.parseInt(fileContents.get(TOTAL_SUM_INDEX));
         // Make the start points. At the beginning, every planet (0 - count-1) is a start point
-        // This number will be reduced shortly
-        List<Integer> startPoints = new ArrayList<>();
-        for (int i = 0; i < planetCount; ++i) {
-            startPoints.add(i);
-        }
+        // Since the default value is 'false', we use that as '= is a start planet'
+        boolean[] hasInputHyperroutes = new boolean[planetCount];
+        // We're doing a mix of array and lists, since list remove time isn't that good, and we'd only
+        // need to property 'size', we're just keeping track of removals and the list size.
+        int druidStartPlanets = planetCount;
 
         // Read lines planetCount lines, but go from 1 - count instead of 0 - count-1
         // to align with the index, the 0th line was the number of planets, remember?
@@ -43,14 +46,16 @@ public final class OneAndOnly {
             // planet won't have entry degree of 0 anymore, since, you know, we're connecting to it.
             for (int j = 1; j <= routeCount; ++j) {
                 int targetPlanet = Integer.parseInt(numbers[j]);
-                // This cast to object is necessary because otherwise we're calling
-                // list.remove(index), which is a) not what we want and b) returns a T, not a boolean.
-                startPoints.remove((Object) targetPlanet);
+                if (!hasInputHyperroutes[targetPlanet]) {
+                    // Note that this now has >= 1 incoming hyperroutes instead of 0.
+                    // So, we lose a possible start planet.
+                    hasInputHyperroutes[targetPlanet] = true;
+                    druidStartPlanets--;
+                }
             }
         }
 
-        // Droid count = number of points no planet links to = number of remaining start points
-        // Done.
-        System.out.println(startPoints.size());
+        // See how many start planets remain.
+        System.out.println(druidStartPlanets);
     }
 }
